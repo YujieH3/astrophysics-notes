6c064507db3cb8f770b4cce4233b10d24d33317d
Yujie He <yujie.jay.he@outlook.com>
2023-12-28 16:23:28 +0100

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -1,6 +1,11 @@
 export default {};
 
-// Add table of content sidebar
+// custom configurations
 const config = {
-  showSidebar: true,
+  showSidebar: true, // Add a table of content sidebar
+  search: {          // Add a search bar
+    provider: "kbar", 
+  },
+  // navLinks: [{ href: "/about", name: "About" }], // Add a navigation bar soon
+
 };
