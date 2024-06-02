760d22491d55a9347b8dbea5fb2dd5efce92184b
Yujie He <yujie.jay.he@outlook.com>
2024-06-02 23:12:35 +0200

    Update config.mjs. The all checks has failed is due to some daily limit of free hobby user of Vercel. Not a bug.

--- content/config.mjs
+++ content/config.mjs
@@ -2,17 +2,17 @@
 
 // custom configurations
 const config = {
-  // showSidebar: true, // Add a table of content sidebar
-  // search: {          // Add a search bar
-  //  provider: "kbar", 
-  // },
-  // title: "My Notes on Astrophysics",
-  // description: "",
-  // author: "Yujie 'Jay' He",
+  showSidebar: true, // Add a table of content sidebar
+  search: {          // Add a search bar
+    provider: "kbar", 
+  },
+  title: "My Notes on Astrophysics",
+  description: "",
+  author: "Yujie 'Jay' He",
   // logo: "/images/logo.svg",
   // domain: "https://john.app/",
   // links to the pages you want to link to in the navbar and in the footer
-  // navLinks: [{ href: "https://yujieh3.github.io", name: "About" }],
+  navLinks: [{ href: "https://yujieh3.github.io", name: "About" }],
   
 };
 
