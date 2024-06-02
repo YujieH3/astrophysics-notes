d8b7e3985cb6a1a93bf545dc9443746ecdf010af
Yujie He <yujie.jay.he@outlook.com>
2024-06-02 23:05:23 +0200

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -6,13 +6,13 @@ const config = {
   search: {          // Add a search bar
     provider: "kbar", 
   },
-  title: "My Notes on Astrophysics",
-  description: "",
-  author: "Yujie 'Jay' He",
+  // title: "My Notes on Astrophysics",
+  // description: "",
+  // author: "Yujie 'Jay' He",
   // logo: "/images/logo.svg",
   // domain: "https://john.app/",
   // links to the pages you want to link to in the navbar and in the footer
-  navLinks: [{ href: "https://yujieh3.github.io", name: "About" }],
+  // navLinks: [{ href: "https://yujieh3.github.io", name: "About" }],
   
 };
 
