9c7110decc3d2dbf8a1d76023ba9a691265c3919
Yujie He <yujie.jay.he@outlook.com>
2025-06-15 14:22:20 +0200

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -8,8 +8,8 @@ const config = {
   },
   title: "Yujie He",
   description: "",
-  // author: "Yujie He",
-  // logo: "/images/logo.svg",
+  author: "Yujie He",
+  logo: "/images/logo.svg",
   // domain: "https://john.app/",
   // links to the pages you want to link to in the navbar and in the footer
   navLinks: [{ href: "https://yujieh3.github.io", name: "About" }],
